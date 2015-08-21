5364c2e0981662750ec8419f5d13632da9b1db04
Paul Macias <defgarden@gmail.com>
2015-08-20 17:38:08 -0700

    All problems working for small numbers, problem3 hangs with huge numbers.

--- problems.js
+++ problems.js
@@ -42,4 +42,50 @@ function problem_2 () {
 function problem_3 () {
 	// body...
 	var arg = document.getElementById("primeInput").value;
+
+	var primes = [2];
+	var total = 0;
+	var largest_factor = arg;
+	var bool = true;
+
+	for(var i = 3; i <= arg; i++){
+		bool = true;
+		for(var j = 2; j < i; j++){
+			if(i % j == 0){
+				bool = false;
+			}
+		}
+		if(bool == true){
+			total = primes.push(i);
+		}
+	}
+
+	console.log(primes);
+	var iterations = 0;
+	console.log(primes.indexOf(parseInt(largest_factor, 10)));
+	console.log((primes.indexOf(parseInt(largest_factor, 10))) > -1);
+
+	
+	while((primes.indexOf(parseInt(largest_factor, 10)) > -1) === false){
+		var k = 0;
+		console.log(primes.length);
+		while(k < primes.length){
+			if(largest_factor % primes[k] == 0){
+				largest_factor = largest_factor / primes[k];
+				k = primes.length;
+				console.log("largest_factor divided");
+			}
+			else{
+				k++;
+			}
+		}
+		console.log(iterations);
+		iterations++;
+	}
+	
+
+	alert("The largest prime factor of " + arg + " is: " + largest_factor + ".");
+	
+
+	
 }
